10d4737c095fbf1b71844e92f5b8390c5e032937
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 15:31:35 -0700

    isAuth & isAdmin added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -55,3 +55,22 @@ exports.requireSignin = expressJwt({
     userProperty: 'auth'
 });
 
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: 'Access denied'
+        });
+    }
+    next();
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resourse! Access denied'
+        });
+    }
+    next();
+};
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSignin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
 const { userById } = require('../controllers/user');
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({ user: req.profile });
 });
 
