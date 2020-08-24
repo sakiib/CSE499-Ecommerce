c58d93692690aa6304242169828830ad145dfeb0
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 15:52:39 -0700

    create category added

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { create } = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 
-router.post('/category/create', create);
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+router.param('userId', userById);
 
 module.exports = router;
