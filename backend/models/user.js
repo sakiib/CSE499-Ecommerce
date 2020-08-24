5e9ed2a53428847e7a1b299540c4e52ed0f5fc35
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 13:22:57 -0700

    user signin using jwt

--- backend/models/user.js
+++ backend/models/user.js
@@ -48,6 +48,10 @@ userSchema.virtual('password')
 });
 
 userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+    
     encryptPassword: function(password) {
         if (!password) return '';
         try {
