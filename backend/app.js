95d6ce6752e81eaee3c7723754d2c8e2ba5d7500
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 19:17:04 -0700

    cors installed

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 
 // config dotenv
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,3 +9,44 @@ exports.userById = (req, res, next, id) => {
         next();
     });
 };
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.update = (req, res) => {
+    const { name, password } = req.body;
+
+    User.findOne({ _id: req.profile._id }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({ error: 'User not found' });
+        }
+        if (!name) {
+            return res.status(400).json({ error: 'Name is required' });
+        } else {
+            user.name = name;
+        }
+
+        if (password) {
+            if (password.length < 6) {
+                return res.status(400).json({
+                    error: 'Password should be min 6 characters long'
+                });
+            } else {
+                user.password = password;
+            }
+        }
+
+        user.save((err, updatedUser) => {
+            if (err) {
+                console.log('USER UPDATE ERROR', err);
+                return res.status(400).json({ error: 'User update failed' });
+            }
+            updatedUser.hashed_password = undefined;
+            updatedUser.salt = undefined;
+            res.json(updatedUser);
+        });
+    });
+};
