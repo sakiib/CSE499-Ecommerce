30da094d676a4ec7a01757cb35e92d55424b9336
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 21:45:37 -0700

    added dotenv & config for API

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '../core/Layout';
+import {API} from '../config';
 
 const Signup = () =>  (
     <Layout title="Signup Page" description="Sigup to MERN E-commerce App">
-        ...
+        { API }
     </Layout>
 );
 
