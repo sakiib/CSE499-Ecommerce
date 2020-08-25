377447bbb028a73a1ddd905d6c36222d1ec15b68
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 17:03:28 -0700

    product crud

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -4,6 +4,23 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: 'product not found'
+            });
+        } 
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -14,9 +31,23 @@ exports.create = (req, res) => {
             });
         }
 
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+
         let product = new Product(fields);
 
         if (files.photo) {
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
         }
@@ -24,6 +55,58 @@ exports.create = (req, res) => {
         product.save((err, result) => {
             if (err) {
                 console.log('PRODUCT CREATE ERROR ', err);
+                return res.status(400).json({ error: errorHandler(err) });
+            }
+            res.json(result);
+        });
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({ error: errorHandler(err) });
+        }
+        res.json({ message: 'Product deleted successfully' });
+    });
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+
+        // check for all fields
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required'
+            });
+        }
+        
+        let product = req.product;
+        product = _.extend(product, fields);
+
+        if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
                 return res.status(400).json({ error: errorHandler(err) });
             }
             res.json(result);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { create, productById, read, remove, update } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.get('/product/:productId', read);
+router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
