5bb3c0b2a340529182b4f5dc80a8cf99e25511f8
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 19:07:43 -0700

    added photo middleware

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -216,5 +216,13 @@ exports.listBySearch = (req, res) => {
         });
 };
 
+exports.photo = (req, res, next) => {
+    if (req.product.photo.data) {
+        res.set('Content-Type', req.product.photo.contentType);
+        return res.send(req.product.photo.data);
+    }
+    next();
+};
+
 
 
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
+const { 
+    create, productById, read, remove, update, list, 
+    listRelated, listCategories, listBySearch, photo 
+} = require("../controllers/product");
+
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -12,7 +16,8 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
-router.post("/products/by/search", listBySearch);
+router.post('/products/by/search', listBySearch);
+router.get('product/photo/:productId', photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
