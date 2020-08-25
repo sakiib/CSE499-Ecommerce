3119faecd8a1c79b041287374ddc86dd43bffb0a
sakibalamin <sakibalamin162@gmail.com>
2020-08-24 19:03:23 -0700

    product search

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { create, productById, read, remove, update } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -9,6 +9,10 @@ router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.param("userId", userById);
 router.param("productId", productById);
