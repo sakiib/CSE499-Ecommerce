4b26d2981c9eaff32de7f6842000fb7aff86aa2e
sakibalamin <sakibalamin162@gmail.com>
2020-08-27 00:03:22 +0600

    added apiCore for getProduct

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
+import {getProducts} from './apiCore';
 
-const Home = () =>  (
-    <Layout title="Home Page" description="MERN E-commerce App">
-        ...
-    </Layout>
-);
+const Home = () =>  {
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadProductsBySell = () => {
+        getProducts('sold').then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsBySell(data);
+            }
+        });
+    };
+
+    const loadProductsByArrival = () => {
+        getProducts('createdAt').then(data => {
+            console.log(data);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setProductsByArrival(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadProductsByArrival();
+        loadProductsBySell();
+    }, []);
+
+    return (
+        <Layout title="Home Page" description="MERN E-commerce App">
+            {JSON.stringify(productsByArrival)}
+            <hr />
+            {JSON.stringify(productsBySell)}
+        </Layout>
+    );
+};
 
 export default Home;
