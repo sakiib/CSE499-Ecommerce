3d30563d3ad478496ffc703e449b962bff5c446b
sakibalamin <sakibalamin162@gmail.com>
2020-08-27 00:27:29 +0600

    show products in Card

---
+++ frontend/src/core/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Card = ({product}) => {
+    return (
+        <div className="col-4 mb-3">
+            <div className="card">
+                <div className="card-header"> {product.name} </div>
+                <div className="card-body"> 
+                    <p> {product.description} </p>
+                    <p> {product.price} tk </p>
+                    <Link to="/"> 
+                        <button className="btn btn-outline-primary mt-2 mb-2">
+                            View Product
+                        </button>
+                        <button className="btn btn-outline-warning mt-2 mb-2">
+                            Add to cart
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
+
--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import Layout from './Layout';
 import {getProducts} from './apiCore';
+import Card from './Card';
 
 const Home = () =>  {
     const [productsBySell, setProductsBySell] = useState([]);
@@ -34,10 +35,15 @@ const Home = () =>  {
     }, []);
 
     return (
-        <Layout title="Home Page" description="MERN E-commerce App">
-            {JSON.stringify(productsByArrival)}
-            <hr />
-            {JSON.stringify(productsBySell)}
+        <Layout title="Home Page" description="MERN E-commerce App" className="container-fluid">
+            <h2 className="mb-4"> New Arrivals </h2>
+            <div className="row"> 
+                {productsByArrival.map((product, i) => (<Card key={i} product={product}/> ))}
+            </div>
+            <h2 className="mb-4"> Best Sellers </h2>
+            <div className="row">
+                {productsBySell.map((product, i) => (<Card key={i} product={product}/> ))}
+            </div>
         </Layout>
     );
 };
